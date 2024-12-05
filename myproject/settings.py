61077095e4dd11d6ca276193b9c10886fc96eb18
Ghazal Hafezi <ghazalhafezi@Ghazals-Laptop.local>
2024-12-05 09:43:40 +0330

    change some settings for the project

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'testapp',
 ]
-
+INSTALLED_APPS += ['django_celery_results']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysafir',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',  # Use '127.0.0.1' or your server's IP
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
@@ -123,7 +128,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'amqp://localhost'  # RabbitMQ broker
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+  # RabbitMQ broker
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_RESULT_BACKEND = 'django-db'
