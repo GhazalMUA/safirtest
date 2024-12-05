1749cb543cbfe62833bac1c79075873848a91372
Ghazal Hafezi <ghazalhafezi@Ghazals-Laptop.local>
2024-12-05 09:33:33 +0330

    selenium.delay updating fields change insert to whatever i want in future

--- testapp/views.py
+++ testapp/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse
 from .forms import OrderForm
-
+from .tasks import run_selenium_bot
+   
 def create_order(request):
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('order_success')  # Define a success URL or page
+            order = form.save()
+            if order.id:  # Ensure order has been saved and has an id
+                run_selenium_bot.delay('testapp_createorder', order.id,'INSERT')  # Call the Celery task asynchronously
+            else:
+                print("Error: Order ID is missing!")
+            return HttpResponse('Order created successfully and task triggered!')
     else:
         form = OrderForm()
-    return render(request, 'orders/create_order.html', {'form': form})
+    return render(request, 'create_order.html', {'form': form})
+
